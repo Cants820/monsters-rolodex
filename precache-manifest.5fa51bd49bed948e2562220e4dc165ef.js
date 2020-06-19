1951522971db8b6363a4aebd150f9514311a415c
Joseph Cantos <joseph.cantos@hoegh.com>
2020-06-19 13:14:05 +0800

    Updates

---
+++ precache-manifest.5fa51bd49bed948e2562220e4dc165ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d08a71c8fb92e53ad9cd3b1468ae7d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa1e45ace301099898e8",
+    "url": "/monsters-rolodex/static/css/main.54cc3d92.chunk.css"
+  },
+  {
+    "revision": "0d35040140e440a26978",
+    "url": "/monsters-rolodex/static/js/2.1313f230.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.1313f230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1e45ace301099898e8",
+    "url": "/monsters-rolodex/static/js/main.eeb01e78.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
